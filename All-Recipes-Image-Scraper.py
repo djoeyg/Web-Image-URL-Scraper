94da9a6c0969df94b2af1a5643a5741dd8e2335e
djoeyg <greend3@oregonstate.edu>
2021-11-08 23:41:36 -0800

    Formatted All-Recipes-Image-Scraper.py to handle nested json input data and return result data in nested json body.

--- All-Recipes-Image-Scraper.py
+++ All-Recipes-Image-Scraper.py
@@ -7,6 +7,11 @@ from flask_restful import Resource, Api
 app = Flask(__name__)
 api = Api(app)
 
+# Example POST requests in the terminal using curl:
+# curl -H "Content-Type: Application/json" http://127.0.0.1:5000/ -X POST -d "{\"recipe\": {\"name\":\"Tango-Tofu-Tacos\",\"recipe_url\":\"https://www.allrecipes.com/recipe/228966/mango-tofu-tacos/\",\"image_url\":null}}"
+# curl -H "Content-Type: Application/json" http://127.0.0.1:5000/ -X POST -d "{\"recipe\": {\"name\":\"Chicken-Marsala-With-Portobello-Mushrooms\",\"recipe_url\":\"https://www.allrecipes.com/recipe/72405/chicken-marsala-with-portobello-mushrooms/\",\"image_url\":null}}"
+# curl -H "Content-Type: Application/json" http://127.0.0.1:5000/ -X POST -d "{\"recipe\": {\"name\":\"Juicy-Roasted-Chicken\",\"recipe_url\":\"https://www.allrecipes.com/recipe/83557/juicy-roasted-chicken/\",\"image_url\":null}}"
+
 
 class NotFoundError(Exception):
     pass
@@ -17,21 +22,19 @@ class RecipeImage(Resource):
     def post(self):
         try:
             input_json = request.get_json()
-
-            res = requests.get(input_json['url'])
+            res = requests.get(input_json['recipe']['recipe_url'])
             soup = bs4.BeautifulSoup(res.text, 'lxml')
-
             search = soup.find(attrs={"class": "icon-image-zoom"})
             if search is None:
                 raise NotFoundError
-
             image_link = search.get('data-image')
-            # print(image_link)
-            return jsonify({'img_url': image_link})
+            input_json['recipe']['image_url'] = image_link
+            return jsonify(input_json)
+            # return jsonify({'image_url': image_link})
 
         except NotFoundError:
             input_json = request.get_json()
-            abort(404, description=f"Unable to retrieve image of recipe from {input_json['url']}")
+            abort(404, description=f"Unable to retrieve image of recipe from {input_json['recipe']['recipe_url']}")
 
 
 api.add_resource(RecipeImage, '/')
@@ -39,8 +42,3 @@ api.add_resource(RecipeImage, '/')
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5050))
     app.run(port=port, debug=True)
-
-    # Example POST request using curl:
-    # curl -H "Content-Type: Application/json" http://127.0.0.1:5000/ -X POST -d "{\"url\":\"https://www.allrecipes.com/recipe/228966/mango-tofu-tacos/\"}"
-    # curl -H "Content-Type: Application/json" http://127.0.0.1:5000/ -X POST -d "{\"url\":\"https://www.allrecipes.com/recipe/72405/chicken-marsala-with-portobello-mushrooms/\"}"
-    # curl -H "Content-Type: Application/json" http://127.0.0.1:5000/ -X POST -d "{\"url\":\"https://www.allrecipes.com/recipe/83557/juicy-roasted-chicken/\"}"
